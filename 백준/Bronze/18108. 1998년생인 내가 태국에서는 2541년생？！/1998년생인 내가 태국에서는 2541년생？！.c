f66750a21ceb92254abb9b1dc8d0e76b437edff7
SulfurCamp <jalerx6809@gmail.com>
2025-04-14 14:03:58 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+                                                                                                                           
+int main(void){
+    int be;
+    scanf("%d", &be);
+    printf("%d\n", be - 543);
+    
+    return 0;
+}
