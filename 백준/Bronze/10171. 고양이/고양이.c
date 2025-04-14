2454647664e7144aaf22726c8e456bb0245ec574
SulfurCamp <jalerx6809@gmail.com>
2025-04-14 15:10:28 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,16 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+/*
+10171_고양이
+1116KB	0ms
+*/
+#include <stdio.h>
+
+int main() {
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  ) \n");
+	printf(" \\(__)|\n");
+	return 0;
+}
