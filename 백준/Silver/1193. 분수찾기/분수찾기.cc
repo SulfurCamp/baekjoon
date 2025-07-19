05092318873d5eba30086b73cb57c81b358123ee
SulfurCamp <jalerx6809@gmail.com>
2025-07-19 18:55:44 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	int i = 1;
+	
+	while (N > i)
+	{
+		N -= i;
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		cout << i + 1 - N << "/" << N;
+	}
+	else
+	{
+		cout << N << "/" << i + 1 - N;
+	}
+	return 0;
+}
