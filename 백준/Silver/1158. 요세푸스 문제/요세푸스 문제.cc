765ebf7cfc442aceff1734c20e14fc5c03874c05
SulfurCamp <jalerx6809@gmail.com>
2025-07-24 19:28:59 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,38 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-#define MAX 5000
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
     int N, K;
-    scanf("%d %d", &N, &K);
+    cin >> N >> K;
 
-    int queue[MAX];
-    int front = 0;
-    int rear = N;
+    vector<int> people;
+    for (int i = 1; i <= N; ++i) {
+        people.push_back(i); 
+    }
+
+    vector<int> result;
+    int idx = 0;
+
+    while (!people.empty()) {
+        idx = (idx + K - 1) % people.size(); 
+        result.push_back(people[idx]);      
+        people.erase(people.begin() + idx); 
+    }
+    cout << "<";
     for (int i = 0; i < N; i++) {
-        queue[i] = i + 1;
+        cout << result[i];
+        if (i != N - 1)
+            cout << ", ";
     }
+    cout << ">" << endl;
 
-    printf("<");
-
-    int cnt = 0; // 죽인 사람 수
-    while (cnt < N) {
-        // K-1명을 회전 (맨 앞을 뒤로 보냄)
-        for (int i = 1; i < K; i++) {
-            queue[rear % MAX] = queue[front % MAX];  // 맨 앞 원소를 뒤에 추가
-            front++;
-            rear++;
-        }
-
-        // K번째 사람 제거 (출력)
-        printf("%d", queue[front % MAX]);
-        front++;
-        cnt++;
-
-        if (cnt < N) printf(", ");
-    }
-
-    printf(">\n");
     return 0;
 }
