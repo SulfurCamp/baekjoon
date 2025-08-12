989df11d8ad5e70eaf70ab6dba04643614d7f1dd
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:39:34 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    while (t--) {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << a + b << std::endl;
+    }
+    return 0;
+}
