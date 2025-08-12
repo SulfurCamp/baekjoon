2453a7b791a7cf63a6125356d14f6f17ed616ff6
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:37:49 +0900

    [Bronze III] Title: 최소, 최대, Time: 88 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    cout << *min_element(arr.begin(), arr.end()) << " " << *max_element(arr.begin(), arr.end()) << "\n";
+    return 0;
+}
