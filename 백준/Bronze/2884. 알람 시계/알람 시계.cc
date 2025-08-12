abaacc8134ec36d63760bf1844d97adc78aaa352
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:32:05 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int h, m;
+    std::cin >> h >> m;
+    m -= 45;
+    if (m < 0) {
+        m += 60;
+        h -= 1;
+        if (h < 0) {
+            h = 23;
+        }
+    }
+    std::cout << h << " " << m << std::endl;
+    return 0;
+}
