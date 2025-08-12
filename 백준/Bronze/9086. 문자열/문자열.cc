f99aa8afa352f7f6a00865f640cc583ffe2875b6
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:34:13 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        cout << s.front() << s.back() << "\n";
+    }
+    return 0;
+}
