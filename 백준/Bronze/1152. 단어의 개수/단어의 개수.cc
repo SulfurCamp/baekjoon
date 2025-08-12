990886eda8bbeda9ad5d66c3ca02fd58dc063b26
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:23:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 48 ms, Memory: 5964 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    string line;
+    getline(cin, line);
+    stringstream ss(line);
+    string word;
+    int count = 0;
+    while (ss >> word) count++;
+    cout << count << "\n";
+    return 0;
+}
