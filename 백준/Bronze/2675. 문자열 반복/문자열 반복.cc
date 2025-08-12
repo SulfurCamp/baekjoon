79154efa992aba57d4b38ccab3a42dbb8abc1ded
SulfurCamp <jalerx6809@gmail.com>
2025-08-12 10:29:07 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int r;
+        string s;
+        cin >> r >> s;
+        for (char c : s)
+            for (int i = 0; i < r; ++i)
+                cout << c;
+        cout << "\n";
+    }
+    return 0;
+}
