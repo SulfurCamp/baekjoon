7ce9d4106b50223684d81f69b52cda94830b8599
SulfurCamp <jalerx6809@gmail.com>
2025-08-24 17:45:03 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,58 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
-int bfs_count(const vector<vector<int>>& computer, int first)
-{
-    int n = (int)computer.size();
-    vector<bool> visit(n, false);
-
-    queue<int> queue;
-    queue.push(first);
-
-    int current = 0;
-
-    while(!queue.empty())
-    {
-        int k = queue.front();
-        queue.pop();
-    
-        for(int v : computer[k])
-        {
-            if(!visit[v])
-            {
-                visit[v] = true;
-                queue.push(v);
-
-                if(v != first)
-                    current++;
-            }
-        }
-    }
-    return current;
-}
-
-int main()
-{
+int main() {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> computer(N+1);
-
-    for(int i = 0; i < M; i++)
-    {
+    vector<vector<int>> computer(N + 1);
+    for (int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
         computer[a].push_back(b);
         computer[b].push_back(a);
     }
 
+    vector<bool> visited(N + 1, false);
     const int first = 1;
-    int answer = bfs_count(computer, first);
+    int answer = 0;
+
+    stack<int> st;
+    st.push(first);
+    visited[first] = true;
+
+    while (!st.empty()) {
+        int cur = st.top();
+        st.pop();
+
+        for (int nxt : computer[cur]) {
+            if (!visited[nxt]) {
+                visited[nxt] = true;
+                if (nxt != first) answer++;
+                st.push(nxt);
+            }
+        }
+    }
 
     cout << answer << '\n';
     return 0;
-
 }
